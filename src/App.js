f5ab22fde33fca6b8996e7a3a97cc4ed0fe4a3e1
baekjung <zvgandam@gmail.com>
2020-11-25 18:13:45 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import ReactPlayer from "react-player/youtube";
+import useAxios from "axios-hooks";
+import Loader from "components/Loader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const Container = styled.div`
+  position: relative;
+`;
+
+const App = () => {
+  const [{ data, loading }] = useAxios(
+    `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_KEY}&part=snippet&eventType=live&type=video&channelId=UCt91xMmbhyLSbvslF7e_Mrg`
   );
-}
+
+  if (loading) return <Loader />;
+
+  return (
+    <Container>
+      {data.items?.map((item) => (
+        <div key={item?.id.videoId}>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${item?.id.videoId}`}
+            width="50%"
+            controls
+            playing
+            muted
+          />
+          <span>{item.snippet.title}</span>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Loader as LoaderComponent } from "semantic-ui-react";
+
+const Loader = () => {
+  return <LoaderComponent active inline="centered" />;
+};
+
+export default Loader;
